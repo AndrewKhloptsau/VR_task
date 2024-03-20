716f5b48f06fc90bff65fcd94c1b3775ccc6606f
RedLeonFire <redleonfire55.ak@gmail.com>
2024-03-20 22:57:59 +0200

    Parser method has been added

--- src/DataWorkers/Reader/DataReader.cs
+++ src/DataWorkers/Reader/DataReader.cs
@@ -1,10 +1,25 @@
 ﻿using NLog;
+using System.Collections.Frozen;
 
 namespace VRtask.DataWorkers.Reader
 {
+    internal enum DataType : byte
+    {
+        Unknown = 0,
+        Box,
+        Content,
+    }
+
+
     internal sealed class DataReader : IDataReader
     {
         private const int ReadBufferSize = 1 << 12; //4096
+
+        private static readonly FrozenDictionary<string, DataType> _lineTypeMapper = new Dictionary<string, DataType>()
+        {
+            ["HDR"] = DataType.Box,
+            ["LINE"] = DataType.Content,
+        }.ToFrozenDictionary(); //fast lookup for immutable dictionary
 
         private readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
@@ -21,9 +36,15 @@ namespace VRtask.DataWorkers.Reader
                 using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, ReadBufferSize, FileOptions.Asynchronous | FileOptions.SequentialScan);
                 using var reader = new StreamReader(fileStream);
 
+                int lineNumber = 0;
+
                 while (!reader.EndOfStream)
                 {
                     var line = await reader.ReadLineAsync(token);
+                    lineNumber++;
+
+                    if (!TryParseString(line, out var error))
+                        _logger.Error("Parsing error on line #{0}: {1}", lineNumber, error);
                 }
             }
             catch (Exception ex)
@@ -31,5 +52,71 @@ namespace VRtask.DataWorkers.Reader
                 _logger.Error("Cannot process file {0}. Exception {1}", filePath, ex);
             }
         }
+
+
+        private static bool TryParseString(string str, out string error) =>
+            GetLineType(str) switch
+            {
+                DataType.Content => TryParseAndSaveContentInfo(str, out error),
+                DataType.Box => TryParseAndSaveBoxInfo(str, out error),
+
+                _ => SetUnknownError(str, out error)
+            };
+
+        private static bool TryParseAndSaveBoxInfo(string str, out string error)
+        {
+            var parts = GetParts(str);
+
+            if (parts.Length != 3)
+                return SetInvalidStringFormatError(str, out error);
+
+            error = null;
+            return true;
+        }
+
+        private static bool TryParseAndSaveContentInfo(string str, out string error)
+        {
+            var parts = GetParts(str);
+
+            if (parts.Length != 4)
+                return SetInvalidStringFormatError(str, out error);
+
+            var isbnStr = parts[2];
+            var qtyStr = parts[3];
+
+            if (!long.TryParse(isbnStr, out var isbn))
+                return SetInvalidNumberError(isbnStr, out error);
+
+            if (!long.TryParse(qtyStr, out var qty))
+                return SetInvalidNumberError(qtyStr, out error);
+
+            error = null;
+            return true;
+        }
+
+
+        private static bool SetInvalidNumberError(string str, out string error)
+        {
+            error = $"Invalid number {str}";
+            return false;
+        }
+
+        private static bool SetInvalidStringFormatError(string str, out string error)
+        {
+            error = $"Invalid format {str}";
+            return false;
+        }
+
+        private static bool SetUnknownError(string str, out string error)
+        {
+            error = $"Unknown line type {str}";
+            return false;
+        }
+
+        private static DataType GetLineType(ReadOnlySpan<char> span) =>
+            _lineTypeMapper.TryGetValue(span.ToString(), out var type) ? type : DataType.Unknown;
+
+        private static string[] GetParts(string str) =>
+            str.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
     }
 }
